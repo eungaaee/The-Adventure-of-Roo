f00040582d40ce2cbf82d01653afe772e3073806
aidjiwon1004@gmail.com <aidjiwon1004@gmail.com>
2024-05-15 01:06:57 +0900

    Changeset: 17 플레이어 이동 구현 (달리기만)

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,14 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
     public float maxSpeed;
     Rigidbody2D rigid;
+    SpriteRenderer sprRdr;
+    Animator anim;
     void Awake() {
         rigid = GetComponent<Rigidbody2D>();
+        sprRdr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+    void Update() {
+        // 캐릭터 좌우 이동
+        if (Input.GetButtonUp("Horizontal"))
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.1f, rigid.velocity.y);
+
+        // 캐릭터 방향 전환
+        if (Input.GetButton("Horizontal"))
+            // sprRdr.flipX = Input.GetAxisRaw("Horizontal") == -1;
+            sprRdr.flipX = rigid.velocity.x < 0;
+
+        // 달리기 애니메이션
+        if (Mathf.Abs(rigid.velocity.x) < 0.25)
+            anim.SetBool("isRunning", false);
+        else
+            anim.SetBool("isRunning", true);
     }
     void FixedUpdate() {
         float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right*h, ForceMode2D.Impulse);
+        if (rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < -maxSpeed)
+            rigid.velocity = new Vector2(-maxSpeed, rigid.velocity.y);
     }
 }
