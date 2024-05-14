7dd04a5507c43eb963862a0c7cb01414348c002e
aidjiwon1004@gmail.com <aidjiwon1004@gmail.com>
2024-05-15 02:47:36 +0900

    Changeset: 18 플레이어 점프 기능 구현, 떨어지는 애니메이션은 일부만.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
     public float maxSpeed;
+    public float jumpHeight;
     Rigidbody2D rigid;
     SpriteRenderer sprRdr;
     Animator anim;
@@ -14,27 +15,47 @@ public class PlayerMove : MonoBehaviour {
         anim = GetComponent<Animator>();
     }
     void Update() {
-        // 캐릭터 좌우 이동
+        // 좌우 방향키 땠을 때 속력 *0.1
         if (Input.GetButtonUp("Horizontal"))
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.1f, rigid.velocity.y);
 
-        // 캐릭터 방향 전환
+        // 캐릭터 방향 반전
         if (Input.GetButton("Horizontal"))
-            // sprRdr.flipX = Input.GetAxisRaw("Horizontal") == -1;
-            sprRdr.flipX = rigid.velocity.x < 0;
+            sprRdr.flipX = rigid.velocity.normalized.x < 0;
 
-        // 달리기 애니메이션
+        // 좌우 이동 애니메이션
         if (Mathf.Abs(rigid.velocity.x) < 0.25)
             anim.SetBool("isRunning", false);
         else
             anim.SetBool("isRunning", true);
+
+        // 점프 상승
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping")) {
+            rigid.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
     }
     void FixedUpdate() {
+        // 좌우 이동
         float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right*h, ForceMode2D.Impulse);
         if (rigid.velocity.x > maxSpeed)
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         else if (rigid.velocity.x < -maxSpeed)
             rigid.velocity = new Vector2(-maxSpeed, rigid.velocity.y);
+
+        // 점프 착지
+        Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+        if (rigid.velocity.normalized.y < 0) {
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Floor"));
+            // 점프 착지가 아닌 그냥 낙하
+            if (!anim.GetBool("isJumping"))
+                anim.SetBool("isFalling", true);
+            else if (rayHit.collider != null && rayHit.distance < 0.5f) {
+                // Debug.Log(rayHit.collider.name);
+                anim.SetBool("isJumping", false);
+                anim.SetBool("isFalling", false);
+            }
+        }
     }
 }
