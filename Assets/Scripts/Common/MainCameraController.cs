400c309c0d43e6a7091319a014f98088beb688d1
aidjiwon1004@gmail.com <aidjiwon1004@gmail.com>
2024-07-12 19:52:05 +0900

    Changeset: 65 카메라 줌 바운더리 안에 있는지 확인하는 로직을 좌표 계산에서 콜라이더 트리거 확인으로 변경

--- Assets/Scripts/Common/MainCameraController.cs
+++ Assets/Scripts/Common/MainCameraController.cs
@@ -15,6 +15,7 @@ public class MainCameraController : MonoBehaviour {
     private float ZoomAmount;
     private float InitZoomSize;
     private Vector2 InitCameraBoundary;
+    private bool isZoom = false;
 
     void Awake() {
         RooTransform = GameObject.Find("Roo").GetComponent<Transform>();
@@ -23,20 +24,40 @@ public class MainCameraController : MonoBehaviour {
         InitCameraBoundary = maxCameraBoundary;
     }
 
-    void Update() {
-        Vector2 ZBPosition = ZoomBoundary.GetComponent<Transform>().position;
-        Vector2 ZBScale = ZoomBoundary.GetComponent<Transform>().lossyScale;
-        if (transform.position.x >= ZBPosition.x - ZBScale.x/2 
-            && transform.position.x <= ZBPosition.x + ZBScale.x/2) ZoomIn();
-        else ZoomOut();
-    }
-
     void FixedUpdate() {
         Vector3 targetPos = new Vector3(RooTransform.position.x, RooTransform.position.y, transform.position.z);
         targetPos.x = Mathf.Clamp(targetPos.x, minCameraBoundary.x, maxCameraBoundary.x);
         targetPos.y = Mathf.Clamp(targetPos.y, minCameraBoundary.y, maxCameraBoundary.y);
 
         transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * FollowSpeed);
+
+        if (isZoom) Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, InitZoomSize - ZoomAmount, Time.deltaTime*ZoomSpeed);
+        else Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, InitZoomSize, Time.deltaTime*ZoomSpeed);
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.CompareTag("zoomboundary")) {
+            ZoomIn();
+            isZoom = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+        if (col.CompareTag("zoomboundary")) {
+            ZoomOut();
+            isZoom = false;
+        }
+    }
+
+    private void ZoomIn() {
+        maxCameraBoundary.y = InitCameraBoundary.y + ((Vector2)Variables.Object(GameObject.Find("ZoomBoundary")).Get("adjustCameraBoundary")).y;
+
+        ZoomAmount = (float)Variables.Object(GameObject.Find("ZoomBoundary")).Get("ZoomAmount");
+        ZoomSpeed = (float)Variables.Object(GameObject.Find("ZoomBoundary")).Get("ZoomSpeed");
+    }
+
+    private void ZoomOut() {
+        maxCameraBoundary.y = InitCameraBoundary.y;
     }
 
     public IEnumerator Shake() {
@@ -48,18 +69,5 @@ public class MainCameraController : MonoBehaviour {
             yield return null;
         }
         transform.position = initPos;
-    }
-
-    public void ZoomIn() {
-        maxCameraBoundary.y = InitCameraBoundary.y + ((Vector2)Variables.Object(GameObject.Find("ZoomBoundary")).Get("adjustCameraBoundary")).y;
-
-        ZoomAmount = (float)Variables.Object(GameObject.Find("ZoomBoundary")).Get("ZoomAmount");
-        ZoomSpeed = (float)Variables.Object(GameObject.Find("ZoomBoundary")).Get("ZoomSpeed");
-        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, InitZoomSize - ZoomAmount, Time.deltaTime*ZoomSpeed);
-    }
-
-    public void ZoomOut() {
-        maxCameraBoundary.y = InitCameraBoundary.y;
-        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, InitZoomSize, Time.deltaTime*ZoomSpeed);
     }
 }
