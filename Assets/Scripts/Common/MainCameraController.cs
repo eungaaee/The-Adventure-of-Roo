15c6870e6680d249f35b75318bfabea3d04520dd
aidjiwon1004@gmail.com <aidjiwon1004@gmail.com>
2024-07-12 19:09:17 +0900

    Changeset: 64 카메라 줌인-줌아웃 기능 구현 및 동굴에 적용

--- Assets/Scripts/Common/MainCameraController.cs
+++ Assets/Scripts/Common/MainCameraController.cs
@@ -1,17 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MainCameraController : MonoBehaviour {
     public Transform RooTransform;
-    private float followSpeed = 2.5f;
-    public Vector2 minCameraBoundary, maxCameraBoundary;
+    public GameObject ZoomBoundary;
 
+    public Vector2 minCameraBoundary, maxCameraBoundary;
+    public float FollowSpeed = 2.5f;
     private float ShakeDuration = 0.25f;
     private float ShakeAmount = 0.25f;
+    private float ZoomSpeed;
+    private float ZoomAmount;
+    private float InitZoomSize;
+    private Vector2 InitCameraBoundary;
 
     void Awake() {
         RooTransform = GameObject.Find("Roo").GetComponent<Transform>();
+        ZoomBoundary = GameObject.Find("ZoomBoundary");
+        InitZoomSize = Camera.main.orthographicSize;
+        InitCameraBoundary = maxCameraBoundary;
+    }
+
+    void Update() {
+        Vector2 ZBPosition = ZoomBoundary.GetComponent<Transform>().position;
+        Vector2 ZBScale = ZoomBoundary.GetComponent<Transform>().lossyScale;
+        if (transform.position.x >= ZBPosition.x - ZBScale.x/2 
+            && transform.position.x <= ZBPosition.x + ZBScale.x/2) ZoomIn();
+        else ZoomOut();
     }
 
     void FixedUpdate() {
@@ -19,7 +36,7 @@ public class MainCameraController : MonoBehaviour {
         targetPos.x = Mathf.Clamp(targetPos.x, minCameraBoundary.x, maxCameraBoundary.x);
         targetPos.y = Mathf.Clamp(targetPos.y, minCameraBoundary.y, maxCameraBoundary.y);
 
-        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * followSpeed);
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * FollowSpeed);
     }
 
     public IEnumerator Shake() {
@@ -31,5 +48,18 @@ public class MainCameraController : MonoBehaviour {
             yield return null;
         }
         transform.position = initPos;
+    }
+
+    public void ZoomIn() {
+        maxCameraBoundary.y = InitCameraBoundary.y + ((Vector2)Variables.Object(GameObject.Find("ZoomBoundary")).Get("adjustCameraBoundary")).y;
+
+        ZoomAmount = (float)Variables.Object(GameObject.Find("ZoomBoundary")).Get("ZoomAmount");
+        ZoomSpeed = (float)Variables.Object(GameObject.Find("ZoomBoundary")).Get("ZoomSpeed");
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, InitZoomSize - ZoomAmount, Time.deltaTime*ZoomSpeed);
+    }
+
+    public void ZoomOut() {
+        maxCameraBoundary.y = InitCameraBoundary.y;
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, InitZoomSize, Time.deltaTime*ZoomSpeed);
     }
 }
