4cc2cf3378e6ef1b1dfdc55a26b6be6c78f01882
aidjiwon1004@gmail.com <aidjiwon1004@gmail.com>
2024-07-20 14:18:01 +0900

    Changeset: 106 떨어지는 독물 안 맞는 버그 수정

--- Assets/Scripts/FallingBlockController.cs
+++ Assets/Scripts/FallingBlockController.cs
@@ -20,7 +20,7 @@ public class FallingBlockController : MonoBehaviour {
         else StartCoroutine(RayCheck());
     }
 
-    private void OnCollisionEnter2D(Collision2D col) {
+    private void OnCollisionStay2D(Collision2D col) {
         if (IsLoop) StartCoroutine(ResetPos());
         else Destroy(gameObject);
     }
