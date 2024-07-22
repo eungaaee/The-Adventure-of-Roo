2b5a1daa6785d65934afbc121053f8a6e9a4a06c
aidjiwon1004@gmail.com <aidjiwon1004@gmail.com>
2024-07-23 00:58:34 +0900

    Changeset: 114

--- Assets/Scripts/CutScenes/Stage1End.cs
+++ Assets/Scripts/CutScenes/Stage1End.cs
@@ -10,6 +10,7 @@ public class Stage1End : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D col) {
         if (col.gameObject.CompareTag("Player")) {
+            GetComponent<BoxCollider2D>().enabled = false;
             StartCoroutine(StartCutscene());
         }
     }
