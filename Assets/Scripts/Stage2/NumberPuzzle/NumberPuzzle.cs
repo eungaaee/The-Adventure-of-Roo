8b8ca1b243812eab91cb32b417b5b3e0f25e99d7
aidjiwon1004@gmail.com <aidjiwon1004@gmail.com>
2024-08-16 19:26:44 +0900

    Changeset: 146 스테이지2 숫자 퍼즐 그리드를 C# 배열에 저장하기

--- Assets/Scripts/Stage2/NumberPuzzle/NumberPuzzle.cs
+++ Assets/Scripts/Stage2/NumberPuzzle/NumberPuzzle.cs
@@ -1,15 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NumberPuzzle : MonoBehaviour {
+    [SerializeField] private GameObject Roo;
     [SerializeField] private GameObject PeekingRoo;
 
+    private TextMeshPro[,] grid = new TextMeshPro[9, 9];
+    private int curX, curY;
+
+    private void Awake() {
+        Transform[] cells = GetComponentsInChildren<Transform>();
+        int p = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                grid[i, j] = cells[p++].GetComponentInChildren<TextMeshPro>();
+            }
+        }
+
+        curX = 0; curY = 8;
+    }
+
     private void Start() { // 스크립트 인스턴스 활성화 이후 첫 프레임을 업데이트하기 전에 실행됨
+        Roo.SetActive(false);
         PeekingRoo.SetActive(true);
+
     }
 
     void Update() {
-        
+
     }
 }
