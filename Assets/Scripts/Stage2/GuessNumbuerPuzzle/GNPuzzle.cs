6289beb71063582af3bdba68c3357bde28164f32
aidjiwon1004@gmail.com <aidjiwon1004@gmail.com>
2024-08-21 20:40:57 +0900

    Changeset: 167 스테이지2 숫자맞추기퍼즐 - 버튼으로 입력하는 숫자가 문제에 반영되도록 구현

--- Assets/Scripts/Stage2/GuessNumbuerPuzzle/GNPuzzle.cs
+++ Assets/Scripts/Stage2/GuessNumbuerPuzzle/GNPuzzle.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
+using TMPro;
 using UnityEngine;
-
 public class GNPuzzle : MonoBehaviour {
+    [SerializeField] private TextMeshPro answerField;
+
     private int playerAnswer = 0;
     
     private void Update() {
@@ -12,10 +15,22 @@ public class GNPuzzle : MonoBehaviour {
     public void IncreasePlayerAnswer() {
         if (playerAnswer > 999) return;
         playerAnswer++;
+        UpdateAnswerText();
     }
 
     public void DecreasePlayerAnswer() {
         if (playerAnswer < 1) return;
         playerAnswer--;
+        UpdateAnswerText();
+    }
+
+    private void UpdateAnswerText() {
+        string newText = "";
+        foreach (char c in answerField.text) {
+            newText += c;
+            if (c == '=') break;
+        }
+        newText += $" {playerAnswer}";
+        answerField.text = newText;
     }
 }
