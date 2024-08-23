f9e40c3b0e42446a72d03a0ad361259b08e1e5cd
aidjiwon1004@gmail.com <aidjiwon1004@gmail.com>
2024-08-23 17:55:33 +0900

    Changeset: 191 스테이지1 바람 - 바람 세기가 컴퓨터 환경에 따라 다르던 버그 수정. (힘 가하는 함수를 FixedUpdate로 변경)

--- Assets/Scripts/WindTrigger.cs
+++ Assets/Scripts/WindTrigger.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.XR;
 using UnityEngine;
 
 public class WindTrigger : MonoBehaviour {
     [SerializeField] private bool IsRightToLeft = false;
+    [SerializeField] private Rigidbody2D PlayerRigid;
+
     private Vector3 StartOfWind, EndOfWind;
-    private IEnumerator Sweeper;
-    private PlayerController Player;
+    private const float horizontalForce = 5, verticalForce = 10;
+    private Vector2 windForce;
+    private bool sweeper;
 
     private void Awake() {
-        Player = GameObject.Find("Roo").GetComponent<PlayerController>();
-
         StartOfWind = transform.position;
         EndOfWind = transform.position;        
         if (IsRightToLeft) {
@@ -25,26 +27,32 @@ public class WindTrigger : MonoBehaviour {
             EndOfWind.x += transform.lossyScale.x/2 * Mathf.Cos(transform.rotation.z);
             EndOfWind.y += transform.lossyScale.x/2 * Mathf.Sin(transform.rotation.z) + transform.lossyScale.y;
         }
+
+        windForce.x = (EndOfWind.x-StartOfWind.x) * horizontalForce;
+        windForce.y = (EndOfWind.y-StartOfWind.y) * verticalForce;
+
+    }
+
+    private void FixedUpdate() {
+        if (sweeper) PlayerRigid.AddForce(windForce, ForceMode2D.Force);
     }
 
     private void OnTriggerEnter2D(Collider2D col) {
         if (col.CompareTag("Player")) {
-            if (Sweeper != null) StopCoroutine(Sweeper);
-            Sweeper = SweepPlayer();
-            StartCoroutine(Sweeper);
+            sweeper = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D col) {
         if (col.CompareTag("Player")) {
-            StopCoroutine(Sweeper);
+            sweeper = false;
         }
     }
 
-    private IEnumerator SweepPlayer() {
+    /* private IEnumerator SweepPlayer() {
         while (true) {
             Player.GetComponent<Rigidbody2D>().AddForce((EndOfWind-StartOfWind)*3f, ForceMode2D.Force);
             yield return null;
         }
-    }
+    } */
 }
